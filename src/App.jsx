d8fa2ec3e7d583767f70f8785b1021de7f17ef1b
Stefan Wille <github@stefanwille.com>
2021-08-05 09:03:47 +0200

    Add react-infinite-scroller

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-infinite-scroller';
 
 function App() {
   // Create the count state.
